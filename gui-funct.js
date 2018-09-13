0c58d8f6d05ec9902d5ae2556af93f19be14072f
Martin Jackson <martin.a.jackson@gmail.com>
2018-09-13 08:45:23 -0500

    previous testing was Mac and Linux, started testing on Win10

--- gui-funct.js
+++ gui-funct.js
@@ -1,28 +1,54 @@
-function appendToDroidOutput(msg) { getDroidOutput().value += msg; };
-function setStatus(msg)           { getStatus().innerHTML = msg; };
+
+var is = require("electron-is");
+
+// Mac and Linux have Bash shell scripts (so the following would work)
+//        var child = process.spawn('child', ['-l']);
+//        var child = process.spawn('./test.sh');       
+// Win10 with WSL (Windows Subsystem for Linux)  https://docs.microsoft.com/en-us/windows/wsl/install-win10
+//   
+// Win10 with Git-Bash (windows Subsystem for Linux) https://git-scm.com/   https://git-for-windows.github.io/
+//
+
+function appendOutput(msg) { getCommandOutput().value += (msg+'\n'); };
+function setStatus(msg)    { getStatus().innerHTML = msg; };
+
+function showOS() {
+    if (is.windows())
+      appendOutput("Windows Detected.")
+    if (is.macOS())
+      appendOutput("Apple OS Detected.")
+    if (is.linux())
+      appendOutput("Linux Detected.")
+}
 
 function backgroundProcess() {
     const process = require('child_process');   // The power of Node.JS
 
-    // var ls = process.spawn('ls', ['-l']);
-    var ls = process.spawn('./test.sh');
+    showOS();
+    
+    var child = (is.windows()) ? 
+             process.spawn('test.bat') 
+             :
+             process.spawn('/usr/bin/bash', ['./test.sh']);       //   process.spawn('./test.sh'); assumes exec permissions are set       
 
-    ls.stdout.on('data', function (data) {
-      // console.log('stdout: <' + data+'> ');
-          // appendToDroidOutput(data);
-      appendToDroidOutput('stdout: <' + data+'> \n');
+    child.on('error', function(err) {
+      appendOutput('stderr: <'+err+'>' );
     });
 
-    ls.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
+    child.stdout.on('data', function (data) {
+      appendOutput(data);
     });
 
-    ls.on('close', function (code) {
-      // console.log('child process exited with code ' + code);
-          if (code == 0)
-         setStatus('child process complete.');
-          else
-         setStatus('child process exited with code ' + code);
-          getDroidOutput().style.background = "DarkGray";
+    child.stderr.on('data', function (data) {
+      appendOutput('stderr: <'+data+'>' );
+    });
+
+    child.on('close', function (code) {
+        if (code == 0)
+          setStatus('child process complete.');
+        else
+          setStatus('child process exited with code ' + code);
+
+        getCommandOutput().style.background = "DarkGray";
     });
 };
